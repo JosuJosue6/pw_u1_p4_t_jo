640201dd7a65c62d9fc49b4406df2e5a9ddb0822
JosuJosue6 <josuensd@gmail.com>
2023-05-15 00:45:21 -0500

    Tarea 3

---
+++ src/main/java/com/example/demo/PwU1P4TJoApplication.java
@@ -0,0 +1,19 @@
+package com.example.demo;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PwU1P4TJoApplication implements CommandLineRunner{
+
+	public static void main(String[] args) {
+		SpringApplication.run(PwU1P4TJoApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		System.out.println("TAREA 3 :)");
+	}
+
+}
